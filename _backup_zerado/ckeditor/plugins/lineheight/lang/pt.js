13de4578abf3c8f70ca7e67eb12dd367d1c70e0e
Requiao <adilson.requiao@gmail.com>
2022-08-02 20:57:04 -0300

    a

--- _backup_zerado/ckeditor/plugins/lineheight/lang/pt.js
+++
@@ -1,3 +0,0 @@
-CKEDITOR.plugins.setLang('lineheight','pt', {
-    title: 'linha Altura'
-} );
--- _backup_zerado/php_functions/get_time_for_id.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//returns 16 digits
-	function getTimeForId() {
-		$time = microtime();
-		$time = str_replace('0.', '', $time);
-		$time = substr_replace($time, '', 6, 3);
-		return $time;
-	}
-?>
